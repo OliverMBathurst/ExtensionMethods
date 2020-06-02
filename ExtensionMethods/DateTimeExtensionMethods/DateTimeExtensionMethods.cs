86626c47436041737670751894173d66b052259e
OliverMBathurst <oliverbathurst12345@gmail.com>
2020-06-02 14:27:05 +0100

    Added datetime extensions
    Added datetime extensions

---
+++ ExtensionMethods/DateTimeExtensionMethods/DateTimeExtensionMethods.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ExtensionMethods.DateTimeExtensionMethods
+{
+    public static class DateTimeExtensionMethods
+    {
+        public static DateTime FromUnixTime(this long unixTime)
+            => new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(unixTime);
+
+        public static long ToUnixTime(this DateTime date) 
+            => Convert.ToInt64((date - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds);        
+    }
+}
